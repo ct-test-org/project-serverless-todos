948c14911a9efd410cd2f57f5b09fde4334405e5
ctownsdin <townsdin@gmail.com>
2021-12-28 20:23:28 -0800

    tmp changes to make this deployable, works

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,22 +1,47 @@
-import 'source-map-support/register'
+import "source-map-support/register";
 
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
-import * as middy from 'middy'
-import { cors } from 'middy/middlewares'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+// import * as middy from 'middy'
+// import { cors } from 'middy/middlewares'
 
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
-import { getUserId } from '../utils';
+// import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
+// import { getUserId } from '../utils';
 
 // TODO: Get all TODO items for a current user
-export const handler = middy(
-  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    // Write your code here
-    const todos = '...'
+// export const handler = middy(
+//   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+//     // Write your code here
+//     const todos = '...'
 
-    return undefined
+//     return undefined
 
-handler.use(
-  cors({
-    credentials: true
-  })
-)
+// handler.use(
+//   cors({
+//     credentials: true
+//   })
+// )
+
+// TEMPORARY HELLO FUNCTION
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  console.log("Hello, Processing event: ", event);
+
+  // const result = await docClient
+  //     .scan({
+  //         TableName: groupsTable,
+  //     })
+  //     .promise();
+
+  // const items = result.Items;
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      items: ["imagine: items here, HELLO Items."],
+    }),
+  };
+};
